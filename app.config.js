13b5a81cfffc692459b738e2a8e3382243307fe0
harry-forge <harjinder2003.02@gmail.com>
2025-10-26 04:54:01 +0530

    created .env file and removed harcoded vars

---
+++ app.config.js
@@ -0,0 +1,63 @@
+import 'dotenv/config';
+
+export default {
+    expo: {
+        name: "AgroSync",
+        slug: "AgroSync",
+        version: "1.0.0",
+        orientation: "portrait",
+        icon: "./assets/images/boardingImage.png",
+        scheme: "agrosync",
+        userInterfaceStyle: "automatic",
+        newArchEnabled: true,
+
+        ios: {
+            supportsTablet: true
+        },
+
+        android: {
+            adaptiveIcon: {
+                backgroundColor: "#E6F4FE",
+                foregroundImage: "./assets/images/android-icon-foreground.png",
+                backgroundImage: "./assets/images/android-icon-background.png",
+                monochromeImage: "./assets/images/android-icon-monochrome.png"
+            },
+            edgeToEdgeEnabled: true,
+            predictiveBackGestureEnabled: false
+        },
+
+        web: {
+            output: "static",
+            favicon: "./assets/images/boardingImage.png"
+        },
+
+        plugins: [
+            "expo-router",
+            [
+                "expo-splash-screen",
+                {
+                    image: "./assets/images/boardingImage.png",
+                    imageWidth: 200,
+                    resizeMode: "contain",
+                    backgroundColor: "#ffffff",
+                    dark: {
+                        backgroundColor: "#000000"
+                    }
+                }
+            ],
+            "expo-font",
+            "expo-sqlite"
+        ],
+
+        experiments: {
+            typedRoutes: true,
+            reactCompiler: true
+        },
+
+        // ✅ Add your .env variables here
+        extra: {
+            supabaseUrl: process.env.EXPO_SUPABASE_URL,
+            supabaseAnonKey: process.env.EXPO_SUPABASE_ANON_KEY,
+        }
+    }
+};
